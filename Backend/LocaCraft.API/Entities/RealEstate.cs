abe3975f0cafcc0d1efd7ff49a56dea342efb6e6
tan-frederic <tanfrederic33@gmail.com>
2026-07-21 23:42:07 +0200

    Initialize LocaCraft API backend project
    Create a new ASP.NET Core Web API project with foundational structure:
    - Basic application setup and configuration files.
    - Initial `User` and `RealEstate` entity models, including data annotations.
    - Project (`.csproj`) and solution (`.slnx`) files for the API.
    
    This establishes the core backend service for future API development.

---
+++ Backend/LocaCraft.API/Entities/RealEstate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace LocaCraft.API.Entities
+{
+    public class RealEstate
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public int PostalCode { get; set; }
+    }
+}
---
+++ Backend/LocaCraft.API/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocaCraft.API.Entities
+{
+    public class User
+    {
+        [Required, MaxLength(50)]
+        public string Username { get; set; } = null!;
+
+        [Required, MaxLength(50), EmailAddress]
+        public string Mail { get; set; } = null!;
+    }
+}
